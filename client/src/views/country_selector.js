425f5d17b7a0c8a06aa697856579e7d66ed90ca9
siansrd <sianrobinsondavies@gmail.com>
2016-12-15 10:22:34 +0000

    Create Select element

--- client/src/views/country_selector.js
+++ client/src/views/country_selector.js
@@ -2,8 +2,16 @@ var CountrySelector = function() {
   this.render();
 }
 
+CountrySelector.prototype.createSelect = function(){
+  var countrySelector = document.getElementById('select-wrapper');
+  var select = document.createElement('select');
+}
+
+
+
 CountrySelector.prototype.render = function(){
   console.log("CountrySelector")
+  this.createSelect();
 }
 
 module.exports = CountrySelector;
