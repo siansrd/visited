510339b02588e9ffb14887b4fb471a47dfbadc92
siansrd <sianrobinsondavies@gmail.com>
2016-12-15 10:08:04 +0000

    Add call to countries api

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,22 @@
-var UI = require('./views/ui')
+var UI = require('./views/ui');
+
 
 window.onload = function(){
+
+  var url = 'https://restcountries.eu/rest/v1';
+  var request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.onload = function () {
+      if (request.status === 200) {
+          var jsonString = request.responseText;
+          var countries = JSON.parse(jsonString);
+          
+          console.log(countries);
+      }
+  };
+  request.send();
+  
+
   var ui = new UI();
   ui.render();
 }
