f9ef0bd63bae4956b521d2173c69c6fff27e722f
siansrd <sianrobinsondavies@gmail.com>
2016-12-16 12:02:41 +0000

    Setup post and get from database

--- server.js
+++ server.js
@@ -1,15 +1,46 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
 
-// var MongoClient = require('mongodb').MongoClient;
-// var url = 'mongodb://localhost:27017/visited';
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/visited';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 })
 
+app.get('/countries', function(req,res) {
+  // Connection URL
+  MongoClient.connect(url, function(err, db) {
+    var collection = db.collection('countries');
+    collection.find({}).toArray(function(err, docs) {
+      res.json(docs);
+      db.close();
+    });
+  });
+});
 
+app.post('/countries', function(req,res) {
+  console.log('body', req.body);
+  MongoClient.connect(url, function(err, db) {
+    var collection = db.collection('countries');
+    collection.insert(
+      { "country": req.body.country
+        // "name": req.body.name,
+        // "capital": req.body.capital,
+        // "currencies": req.body.currencies,
+        // "languages": req.body.languages,
+        // "latlng": req.body.latlng
+      }
+    );
+    res.status(200).end();
+    db.close();
+  });
+});
 
 
 app.use(express.static('client/build'));
